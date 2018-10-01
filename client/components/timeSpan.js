2b359cdca62636e3fb7ccab3240fb828af5d01e7
Glitch (righteous-cause) <none>
2018-10-01 04:34:23 +0000

    🎯🎗 Updated with Glitch

---
+++ client/components/timeSpan.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import UnorderedList from './unorderedList.js';
+
+const startTimeFormatter = function(time) {
+  if (time == null) {
+    return '6';
+  } 
+  return time;
+}
+
+const endTimeFormatter = function(time) {
+  if (time == null) {
+   return 'EOD'; 
+  }
+  return time;
+};
+
+const TimeSpan = function (props) {
+  if (Array.isArray(props.startTime)) {
+      let toReturn = [];
+      for (var i = 0; i < props.startTime.length; i++) {
+       toReturn.push({
+         startTime: startTimeFormatter(props.startTime[i]),
+         endTime: endTimeFormatter(props.endTime[i])
+       });
+     }
+     return (<UnorderedList>
+             {toReturn.map((times) => <span key={times.startTime}>{startTimeFormatter(times.startTime)} - {endTimeFormatter(times.endTime)}</span>)}
+             </UnorderedList>);
+  } else if (props.startTime == null) {
+     return <span>Anytime</span>;    
+ }
+  return <span>{props.startTime} - {props.endTime}</span>;
+}
+
+export default TimeSpan;
